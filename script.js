61a18d3505b5ff4d7d94a723429cd4fb5a0d0cb9
Christian <christianyuen1@gmail.com>
2023-04-11 00:40:47 -0400

    Added Working Navigation

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 // create an app object and intialize function
 const app = {};
 
+setInterval(() => {
+	d = new Date(); //object of date()
+	hr = d.getHours();
+	min = d.getMinutes();
+	sec = d.getSeconds();
+	hr_rotation = 30 * hr + min / 2; //converting current time
+	min_rotation = 6 * min;
+	sec_rotation = 6 * sec;
+
+	hour.style.transform = `rotate(${hr_rotation}deg)`;
+	minute.style.transform = `rotate(${min_rotation}deg)`;
+	second.style.transform = `rotate(${sec_rotation}deg)`;
+}, 1000);
+
 // Lights - On/Off
 const lights = document.querySelector('.switch');
 
