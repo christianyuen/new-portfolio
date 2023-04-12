e91331c86c940c029e503bf48218edb2ad2ef223
Christian <christianyuen1@gmail.com>
2023-04-12 01:07:35 -0400

    Added Digital Clock

--- script.js
+++ script.js
@@ -1,19 +1,36 @@
 // create an app object and intialize function
 const app = {};
 
-setInterval(() => {
-	d = new Date(); //object of date()
-	hr = d.getHours();
-	min = d.getMinutes();
-	sec = d.getSeconds();
-	hr_rotation = 30 * hr + min / 2; //converting current time
-	min_rotation = 6 * min;
-	sec_rotation = 6 * sec;
+// clock
+function showTime(){
+    var date = new Date();
+    var h = date.getHours(); // 0 - 23
+    var m = date.getMinutes(); // 0 - 59
+    var s = date.getSeconds(); // 0 - 59
+    var session = "AM";
+    
+    if(h == 0){
+        h = 12;
+    }
+    
+    if(h > 12){
+        h = h - 12;
+        session = "PM";
+    }
+    
+    h = (h < 10) ? "0" + h : h;
+    m = (m < 10) ? "0" + m : m;
+    s = (s < 10) ? "0" + s : s;
+    
+    var time = h + ":" + m;
+    document.getElementById("MyClockDisplay").innerText = time;
+    document.getElementById("MyClockDisplay").textContent = time;
+    
+    setTimeout(showTime, 1000);
+    
+}
 
-	hour.style.transform = `rotate(${hr_rotation}deg)`;
-	minute.style.transform = `rotate(${min_rotation}deg)`;
-	second.style.transform = `rotate(${sec_rotation}deg)`;
-}, 1000);
+showTime();
 
 // Lights - On/Off
 const lights = document.querySelector('.switch');
